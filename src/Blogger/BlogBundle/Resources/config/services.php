2dbbf54ec085c5e052e3260c5b3e74a11167d7bc
kmekler <kirby.lodin@gmail.com>
2016-02-02 17:26:56 -0800

    Delete services.php

--- src/Blogger/BlogBundle/Resources/config/services.php
+++
@@ -1,9 +0,0 @@
-use Symfony\Component\DependencyInjection\Definition;
-
-$container->setParameter('app.verify_email.username', 'labroots_nathan');
-$container->setParameter('app.verify_email.password', 'wwvvlqhc7tfmpl');
-
-$container->setDefinition('app.verify_email', new Definition(
-    'Blogger\BlogBundle',
-    array('verify_email')
-));
